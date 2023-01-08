87b4ed096516c88676cb386f5b6b899e8783d06d
Andres Reyes <unas.andres2000@gmail.com>
2023-01-08 02:49:25 -0500

    Feat: Refinamiento Models

--- app/Models/Asistencia.php
+++ app/Models/Asistencia.php
@@ -7,5 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Asistencia extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    protected $table = 'asistencia';
+    public $timestamps = false;
+
+    protected $fillable = [
+        "cedula_empleado",
+        "fecha_asistencia_entrada",
+        "fecha_asistencia_salida"
+    ];
+
+    public $rules = [
+        "cedula_empleado" => 'required|numeric',
+        "fecha_asistencia_entrada" => 'required',
+        "fecha_asistencia_salida" => 'required',
+    ];
+
+    public $messages = [
+        'cedula_empleado.required' => 'Debe ingresar la cedula del empleado',
+        'fecha_asistencia_entrada.required' => 'Debe ingresar los nombres del empleado',
+        'fecha_asistencia_salida.required' => 'Debe ingresar los nombres del empleado',
+    ];
 }
--- app/Models/Empleados.php
+++ app/Models/Empleados.php
@@ -7,5 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empleados extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    protected $table = 'empleados';
+    protected $primaryKey = 'cedula';
+    protected $fillable = [
+        "cedula",
+        "nombres",
+        "apellidos",
+        "edad",
+        "cargo",
+        "no_celular",
+    ];
+
+    public $rules = [
+        "cedula" => 'required|unique|numeric',
+        "nombres" => 'required',
+        "apellidos" => 'required',
+        "edad" => 'required',
+        "cargo" => 'required',
+        "no_celular" => 'required|numeric'
+    ];
+
+    public $messages = [
+        'cedula.required' => 'Debe ingresar la cedula del empleado',
+        'nombres.required' => 'Debe ingresar los nombres del empleado',
+        'apellidos.required' => 'Debe ingresar los apellidos del empleado',
+        'edad.required' => 'Debe ingresar una edad',
+        'cargo.required' => 'Debe ingresar un cargo',
+        'no_celular.required' => 'Debe ingresar un número de celular',
+  ];
+
 }
