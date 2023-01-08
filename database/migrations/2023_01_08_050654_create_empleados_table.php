5af3695edbdbfdcfe690fdd157f5c621ec809586
Andres Reyes <unas.andres2000@gmail.com>
2023-01-08 02:07:46 -0500

    Feat: Migraciones y modelos para tablas de Empleados y Asistencias

---
+++ database/migrations/2023_01_08_050654_create_empleados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->integer('cedula');
+            $table->string('nombres');
+            $table->string('apellidos');
+            $table->integer('edad');
+            $table->string('cargo');
+            $table->integer('no_celular');
+            $table->timestamps();
+            $table->primary('cedula');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
---
+++ database/migrations/2023_01_08_055316_create_asistencia_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asistencia', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cedula_empleado');
+            $table->date('fecha_asistencia_entrada');
+            $table->date('fecha_asistencia_salida');
+            $table->foreign('cedula_empleado')->references('cedula')->on('empleados');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asistencia');
+    }
+};
